19d32a6125571d6b0ae5233898de082eaa1ee11d
alfianchandra2607 <alfianchandra783@gmail.com>
2025-10-15 11:45:35 +0700

    Add project files.

---
+++ HypoCalc/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+	cout << "hitung sisi miring" << endl;
+	cout << "" << endl;
+	cout << "    |\\ " << endl;
+	cout << "    | \\ " << endl;
+	cout << "    |  \\ " << endl;
+	cout << "    |   \\ " << endl;
+	cout << " a  |    \\ c" << endl;
+	cout << "    |     \\ " << endl;
+	cout << "    |      \\ " << endl;
+	cout << "     ------" << endl;
+	cout << "        b" << endl;
+
+	/*cout << "masukan sisi a		: ";
+	double sisi_a;
+	cin >> sisi_a;
+	cout << "masukan sisi b		: ";
+	double sisi_b;
+	cin >> sisi_b;*/
+
+	double sisi_a,sisi_b,sisi_c;
+	cout << "masukan sisi a dan b : ";
+	cin >> sisi_a >> sisi_b;
+	sisi_c = sqrt(pow(sisi_a, 2) + pow(sisi_b, 2));
+	cout << "sisi miring(c) adalah	: " << sisi_c << endl;
+}
---
+++ TempConversion/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+	cout << "konversi suhu" << endl;
+	cout << "" << endl;
+	cout << "input celcius : ";
+	double celcius, reamur, fahrenhite;
+	cin >> celcius;
+	cout << "input celcius : " << celcius << char(248) << "C" << endl;
+	reamur = 4.00 / 5.00 * celcius;
+	cout << "hasil konversi C to R : " << reamur << char(248) << "R" << endl;
+	fahrenhite = (celcius * 9.00 / 5.00) + 32;
+	cout << "hasil konversi C to F : " << fahrenhite << char(248) << "F" << endl;
+}
